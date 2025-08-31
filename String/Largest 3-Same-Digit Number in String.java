615c235f49e687b0a55bc42b60f622cf840a8424
HariKrishnaPasupuleti123 <136449790+HariKrishnaPasupuleti123@users.noreply.github.com>
2025-08-31 12:46:54 +0530

    Create Largest 3-Same-Digit Number in String.java

---
+++ String/Largest 3-Same-Digit Number in String.java
@@ -0,0 +1,14 @@
+//constant sliding window
+class Solution {
+    public String largestGoodInteger(String num) {
+      String max="";
+      for(int i=0;i<num.length()-3;i++){
+      String sub= num.substring(i,i+3);
+      if(sub.charAt(0)==sub.charAt(1) && sub.charAt(1)==sub.charAt(2)){
+            max=sub;
+        }
+      } 
+    }
+    return max;
+}
+}
